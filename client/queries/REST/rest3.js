5184112d93df4516cad71ffe201fc6c884a0a70f
laurenzhbr <laurenz.hbr@web.de>
2024-10-04 09:07:00 +0200

    rest3 client

--- client/queries/REST/rest3.js
+++ client/queries/REST/rest3.js
@@ -5,7 +5,6 @@ const rest_use_case_3 = async () => {
     const actualHost = process.env.HOST || 'localhost:4000';
     let accumulatedMetrics = {};
 
-    // 1. Abfrage aller Modems mit Verbindungstyp "FTTH" und Betriebsstatus "enable"
     const url = `http://${actualHost}/partyManagement/organization?organizationType=Marketing-%20und%20Vertriebspartner&creditRating.ratingScore.gt=750&limit=10&sort=-creditRating.ratingScore`;
     accumulatedMetrics = await fetchMetrics(url, accumulatedMetrics);
 
